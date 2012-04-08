393d90a0625d370414201ac27f2ff96ef7c21ed0
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-04-08 10:24:13 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@110 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

---
+++ kbe/src/examples/network/main.cpp
@@ -0,0 +1,37 @@
+#include "main.hpp"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h> 
+#include <iostream>  
+#include <string>  
+#include <cstring>  
+#include <vector>  
+#include <list>  
+#include <map>  
+#include <assert.h>
+typedef int64_t													int64;
+typedef int32_t													int32;
+typedef int16_t													int16;
+typedef int8_t													int8;
+typedef uint64_t												uint64;
+typedef uint32_t												uint32;
+typedef uint16_t												uint16;
+typedef uint8_t													uint8;
+typedef uint16_t												WORD;
+typedef uint32_t												DWORD;
+typedef uint16													ENTITY_TYPE;											// entity的类别类型定义支持0-65535个类别
+typedef int32													ENTITY_ID;												// entityID的类型
+typedef uint32													SPACE_ID;												// 一个space的id
+typedef uint32													CALLBACK_ID;											// 一个callback由CallbackMgr分配的id
+typedef uint32													COMPONENT_ID;											// 一个服务器组件的id										
+typedef	uint32													TIMER_ID;												// 一个timer的id类型
+typedef uint8													MAIL_TYPE;	
+
+
+
+int main()
+{
+
+	return 0;
+};
