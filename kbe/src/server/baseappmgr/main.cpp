5b26d11a99e896165a608dfaab8a19f614c0d82c
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-05-12 03:02:43 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@220 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/src/server/baseappmgr/main.cpp
+++ kbe/src/server/baseappmgr/main.cpp
@@ -8,7 +8,6 @@ Also see acknowledgements in Readme.html
 You may use this sample code for anything you like, it is not covered by the
 same license as the rest of the engine.
 */
-#include "cstdkbe/cstdkbe.hpp"
 #include "server/kbemain.hpp"
 #include "baseappmgr.hpp"
 
@@ -16,5 +15,7 @@ using namespace KBEngine;
 
 int KBENGINE_MAIN(int argc, char* argv[])
 {
-	return kbeMainT<Baseappmgr>(argc, argv, BASEAPPMGR_TYPE);
+	int ret = kbeMainT<Baseappmgr>(argc, argv, BASEAPPMGR_TYPE);
+	getchar();
+	return ret; 
 }
