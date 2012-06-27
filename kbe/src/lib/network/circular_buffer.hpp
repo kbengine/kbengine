ee91418709e9fa8b6b703d1e1f47963f67d15c72
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-06-27 05:07:21 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@272 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/src/lib/network/circular_buffer.hpp
+++ kbe/src/lib/network/circular_buffer.hpp
@@ -2,12 +2,22 @@
 This source file is part of KBEngine
 For the latest info, see http://www.kbengine.org/
 
-Copyright (c) 2008-2012 kbegine Software Ltd
-Also see acknowledgements in Readme.html
+Copyright (c) 2008-2012 KBEngine.
 
-You may use this sample code for anything you like, it is not covered by the
-same license as the rest of the engine.
+KBEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KBEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #ifndef __CIRCULAR_ARRAY_HPP__
 #define __CIRCULAR_ARRAY_HPP__
 
