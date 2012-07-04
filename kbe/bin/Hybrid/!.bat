7696364f02a6975423582ffe911fc5f933a7c271
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-07-04 09:47:23 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@313 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

---
+++ kbe/bin/Hybrid/!.bat
@@ -0,0 +1,6 @@
+start kbmachine.exe
+start dbmgr.exe
+start baseappmgr.exe
+start cellappmgr.exe
+:: start baseapp.exe
+:: start cellapp.exe
---
+++ kbe/bin/Hybrid/~.bat
@@ -0,0 +1,6 @@
+taskkill /f /t /im kbmachine.exe
+taskkill /f /t /im dbmgr.exe
+taskkill /f /t /im baseappmgr.exe
+taskkill /f /t /im cellappmgr.exe
+taskkill /f /t /im baseapp.exe
+taskkill /f /t /im cellapp.exe
