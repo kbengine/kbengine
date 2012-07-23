e123ca4351e501aed3e1d079cba3d870b5b15444
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-07-23 03:01:12 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@350 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- demo/res/scripts/base/kbengine.py
+++ demo/res/scripts/base/kbengine.py
@@ -25,7 +25,7 @@ def onBaseAppShutDown(state):
 	"""
 	DEBUG_MSG('baseapp将要关闭了! state=%i' % state)
 		
-def onInitialize(isReload):
+def onInit(isReload):
 	"""
 	KBEngine method.
 	当引擎启动后初始化完所有的脚本后这个接口被调用
@@ -34,3 +34,14 @@ def onInitialize(isReload):
 	"""
 	DEBUG_MSG('所有的脚本初始化完毕。游戏初始化... isReload:%s' % isReload)
 
+def onFini():
+	pass
+	
+def onCellAppDeath(addr):
+	pass
+	
+def onGlobalData(key, value):
+	DEBUG_MSG('onGlobalData: %s' % key)
+	
+def onGlobalDataDel(key):
+	DEBUG_MSG('onDelGlobalData: %s' % key)
