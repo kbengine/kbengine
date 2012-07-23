b590d37ef8d4cde52856cd422aeaab82f2c22d38
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-07-23 03:01:31 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@351 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- demo/res/scripts/cell/kbengine.py
+++ demo/res/scripts/cell/kbengine.py
@@ -2,10 +2,21 @@
 import KBEngine
 from KBEDebug import *
 
-def onInitialize(isReload):
+def onInit(isReload):
 	"""
 	KBEngine method.
 	当引擎启动后初始化完所有的脚本后这个接口被调用
 	"""
 	DEBUG_MSG('所有的脚本初始化完毕。游戏初始化...')
 	
+def onGlobalData(key, value):
+	DEBUG_MSG('onGlobalData: %s' % key)
+	
+def onGlobalDataDel(key):
+	DEBUG_MSG('onDelGlobalData: %s' % key)
+	
+def onSpaceData( spaceID, entryID, key, value ):
+	pass
+	
+def onAllSpaceGeometryLoaded( spaceID, isBootstrap, mapping ):
+	pass
