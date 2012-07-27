894ae2844d7d7511094166e958c698b2b6d61b9e
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-07-27 04:29:10 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@368 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/src/lib/network/network_interface.cpp
+++ kbe/src/lib/network/network_interface.cpp
@@ -70,6 +70,7 @@ NetworkInterface::NetworkInterface(Mercury::EventDispatcher * pMainDispatcher,
 			intlisteningInterface, &intEndpoint_, pIntListenerReceiver_);
 	}
 
+	// 如果配置了对外端口范围， 如果范围过小这里extEndpoint_可能没有端口可用了
 	KBE_ASSERT(good());
 
 	if (pMainDispatcher != NULL)
