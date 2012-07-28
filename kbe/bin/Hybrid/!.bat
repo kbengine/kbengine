be94945330ec5c5ea1cc9a82c06ea35ff0cfd667
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-07-28 12:44:07 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@383 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/bin/Hybrid/!.bat
+++ kbe/bin/Hybrid/!.bat
@@ -1,7 +1,10 @@
 start kbmachine.exe
+ping 127.0.0.1 -n 1
 start dbmgr.exe
+ping 127.0.0.1 -n 1
 start baseappmgr.exe
 start cellappmgr.exe
+ping 127.0.0.1 -n 1
 start baseapp.exe
 start baseapp.exe
 start cellapp.exe
