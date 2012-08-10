0235cf081b65f3efa2d8f0592e4a196fc0c2f76b
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-08-10 09:24:15 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@512 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/bin/Hybrid64/start.sh
+++ kbe/bin/Hybrid64/start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-export KBE_ROOT='/root/kbengine/'
-export KBE_RES_PATH='/root/kbengine/kbe/res/;/root/kbengine/demo/;/root/kbengine/demo/res/'
-export KBE_HYBRID_PATH='/root/kbengine/kbe/bin/Hybrid64/'
 
 ./kbmachine&
 sleep 1s
