d2120cc80aabc5da3424b2bfac592d2584b87495
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-08-23 03:56:43 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@610 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/tools/xlsx2py/角色基础属性表.bat
+++ kbe/tools/xlsx2py/角色基础属性表.bat
@@ -1,7 +1,7 @@
 call basePath.bat
 @echo off
 set pydatas=%ktpydatas%/avatar_inittab.py
-set excel1=%ktexcels%/基础表格设计/人物初始化表.xlsx
+set excel1=%ktexcels%/基础表格设计/角色基础属性表.xlsx
 echo on
 python xlsx2py/xlsx2py.py %pydatas% %excel1%
 if not defined ktall (ping -n 30 127.1>nul)
