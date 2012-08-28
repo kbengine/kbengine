a35768ade83527e2e184c01d3e77b2b267d6fdb1
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-08-28 10:03:55 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@669 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- demo/res/scripts/cell/Avatar.py
+++ demo/res/scripts/cell/Avatar.py
@@ -10,3 +10,13 @@ class Avatar(GameObject):
 		
 	def onTimer(self, tid, userArg):
 		DEBUG_MSG("Avatar::onTimer: %i, tid:%i, arg:%i" % (self.id, tid, userArg))
+		
+	def spellTarget(self, srcEntityID, skillID, targetID):
+		"""
+		exposed.
+		对一个目标entity施放一个技能
+		"""
+		if srcEntityID != self.id:
+			return
+			
+		DEBUG_MSG("Avatar::spellTarget(%i):skillID=%i, targetID=%i", self.id, skillID, targetID)
