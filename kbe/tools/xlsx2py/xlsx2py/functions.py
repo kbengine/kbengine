a8a53a85162a487cf255e32c3bddeafedc4314a0
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-08-28 08:01:13 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@664 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- kbe/tools/xlsx2py/xlsx2py/functions.py
+++ kbe/tools/xlsx2py/xlsx2py/functions.py
@@ -57,6 +57,17 @@ def funcTupleInt(mapDict, dctData, chilidDict, data):
 
 	return tuple([int(e) for e in data.split(",") if len(e) > 0])
 
+def funcPos2D(mapDict, dctData, chilidDict, data):
+	"""
+	返回tuple数据
+	"""
+	if data is None or (type(data) == str and len(data) == 0):
+		return ()
+
+	data = str(data)
+
+	return tuple(int(data.split(",")[0], 0, data.split(",")[1]))
+	
 def funcDict(mapDict, dctData, chilidDict, data):
 	"""
 	返回dict数据
