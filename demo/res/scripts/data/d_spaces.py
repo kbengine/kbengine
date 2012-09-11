b7f3ee054eb0f3a1dadbb9a8a3292d195be10db7
kbengine <kbengine@d26bdfbe-e24b-4f15-971f-8e0596e1bc08>
2012-09-11 03:30:41 +0000

    git-svn-id: https://kbengine.svn.sourceforge.net/svnroot/kbengine@766 d26bdfbe-e24b-4f15-971f-8e0596e1bc08

--- demo/res/scripts/data/d_spaces.py
+++ demo/res/scripts/data/d_spaces.py
@@ -1,8 +1,3 @@
 # -*- coding: utf-8 -*-
 
-datas={20001: {'resID': 20001, 'name': '\xe9\x81\x87\xe4\xbb\x99\xe6\x9d\x91', 'entityType': 'Space', 'type': 1, 'entities': (10001, 10002, 10003, 20001, 20002, 20003, 20004, 20005), 'id': 20001}, 20002: {'resID': 20002, 'name': '\xe6\x9d\x91\xe5\xa4\x96\xe6\xa0\x91\xe6\x9e\x97', 'entityType': 'Space', 'type': 1, 'entities': (), 'id': 20002}, 30001: {'resID': 20003, 'name': '\xe5\xa5\xb3\xe5\xa8\xb2\xe6\xb3\x89', 'entityType': 'Space', 'type': 1, 'entities': (10001, 10002, 10003, 20001, 20002, 20003, 20004, 20005), 'id': 30001}, 30002: {'resID': 30001, 'name': '\xe5\xb8\x83\xe5\xba\x84', 'entityType': 'Space', 'type': 2, 'entities': (), 'id': 30002}, 30003: {'resID': 30002, 'name': '\xe8\x8d\xaf\xe5\xba\x97', 'entityType': 'Space', 'type': 2, 'entities': (), 'id': 30003}, 30004: {'resID': 30003, 'name': '\xe9\x93\x81\xe5\x8c\xa0\xe9\x93\xba', 'entityType': 'Space', 'type': 2, 'entities': (), 'id': 30004}}
-
-allDatas = {
-	'场景表':datas,
-	'建筑表':datas,
-}
+datas={5200001: {'resID': 6200001, 'name': '布庄', 'entityType': 'Space', 'entities': (1000004,), 'type': 2, 'id': 5200001}, 5000001: {'resID': 6000001, 'name': '遇仙村', 'entityType': 'Space', 'entities': (1000001, 1000002, 1000003, 1000004, 1000005, 1000006, 1100001, 1100002, 1100003, 1100004), 'type': 1, 'id': 5000001}, 5200002: {'resID': 6200002, 'name': '药店', 'entityType': 'Space', 'entities': (1000005,), 'type': 2, 'id': 5200002}, 5200003: {'resID': 6200003, 'name': '铁匠铺', 'entityType': 'Space', 'entities': (1000006,), 'type': 2, 'id': 5200003}, 5100001: {'resID': 6100001, 'name': '村外树林', 'entityType': 'Space', 'entities': (1200001, 1200002, 1220001), 'type': 1, 'id': 5100001}}
