253ef5388fc266b57a4abbc8b0ac6bf5912ffce7
kbengine <liquidx@163.com>
2012-09-13 17:52:54 +0800

    更新逻辑类容。 准备实现简单的副本管理系统。

--- demo/res/scripts/base/Spaces.py
+++ demo/res/scripts/base/Spaces.py
@@ -5,15 +5,77 @@ from KBEDebug import *
 from GameObject import GameObject
 import d_spaces
 
+class SpaceAlloc:
+	def __init__(self, utype):
+		self._spaces = {}
+		self._utype = utype
+	
+	def init(self):
+		"""
+		"""
+		spaceData = d_spaces.datas.get(self._utype)
+		KBEngine.createBaseAnywhere(spaceData["entityType"], \
+											{"spaceUType" : self._utype}, \
+											self.onSpaceCreatedCB)
+
+	def onSpaceCreatedCB(self, space):
+		"""
+		一个space创建好后的回调
+		"""
+		DEBUG_MSG("Spaces::create space %i. entityID=%i" % (self._utype, space.id))
+		self._spaces[space.id] = space
+		
+	def alloc(self):
+		if self._spaces == {}:
+			return None
+		
+		return list(self._spaces.values())[0]
+		
+	def loginToSpace(self, avatarMailbox):
+		"""
+		某个玩家请求登陆到某个space中
+		"""
+		space = self.alloc()
+		if space is None:
+			ERROR_MSG("Spaces::loginToSpace: not found space %i. login to space is failed!" % self._utype)
+			return
+		
+		space.onLoginToSpace(avatarMailbox)
+
+	def teleportSpace(self, entityMailbox, position, direction):
+		"""
+		请求进入某个space中
+		"""
+		space = self.alloc()
+		if space is None:
+			ERROR_MSG("Spaces::loginToSpace: not found space %i. login to space is failed!" % self._utype)
+			return
+		
+		DEBUG_MSG("Spaces::teleportSpace: entityMailbox=%s" % entityMailbox)
+		space.teleportSpace(entityMailbox, position, direction)
+		
+class SpaceAllocCopy(SpaceAlloc):
+	def __init__(self, utype):
+		SpaceAlloc.__init__(self, utype)
+
+		
 class Spaces(GameObject):
 	def __init__(self):
 		GameObject.__init__(self)
-		self._spaces = {}
+		self._spaceAllocs = {}
 		self.addTimer(3, 1, 1)
-		self._tmpDatas = list(d_spaces.datas.keys())
-		
+		self.initAlloc()
 		KBEngine.globalData["SpaceMgr"] = self
-		
+	
+	def initAlloc(self):
+		self._tmpDatas = list(d_spaces.datas.keys())
+		for utype in self._tmpDatas:
+			spaceData = d_spaces.datas.get(utype)
+			if spaceData["entityType"] == "SpaceCopy":
+				self._spaceAllocs[utype] = SpaceAllocCopy(utype)
+			else:
+				self._spaceAllocs[utype] = SpaceAlloc(utype)
+				
 	def onTimer(self, id, userArg):
 		"""
 		KBEngine method.
@@ -24,42 +86,22 @@ class Spaces(GameObject):
 		if userArg == 1:
 			if len(self._tmpDatas) > 0:
 				spaceUType = self._tmpDatas.pop(0)
-				spaceData = d_spaces.datas.get(spaceUType)
-				space = KBEngine.createBaseAnywhere(spaceData["entityType"], \
-													{"spaceUType" : spaceUType}, \
-													Functor.Functor(self.onSpaceCreatedCB, spaceUType))
+				self._spaceAllocs[spaceUType].init()
 				
 			if len(self._tmpDatas) <= 0:
+				del self._tmpDatas
 				self.delTimer(id)
-				
-	def onSpaceCreatedCB(self, spaceUType, space):
-		"""
-		一个space创建好后的回调
-		"""
-		DEBUG_MSG("Spaces::create space %i. entityID=%i" % (spaceUType, space.id))
-		self._spaces[spaceUType] = space
-		
+			
 	def loginToSpace(self, avatarMailbox, spaceUType):
 		"""
 		define method.
 		某个玩家请求登陆到某个space中
 		"""
-		space = self._spaces.get(spaceUType)
-		if space is None:
-			ERROR_MSG("Spaces::loginToSpace: not found space %i. login to space is failed!" % spaceUType)
-			return
-		
-		space.onLoginToSpace(avatarMailbox)
+		self._spaceAllocs[spaceUType].loginToSpace(avatarMailbox)
 		
 	def teleportSpace(self, entityMailbox, spaceUType, position, direction):
 		"""
 		define method.
 		请求进入某个space中
 		"""
-		space = self._spaces.get(spaceUType)
-		if space is None:
-			ERROR_MSG("Spaces::teleportTo: not found space %i. teleportTo is failed!" % spaceUType)
-			return
-		
-		DEBUG_MSG("Spaces::teleportSpace: entityMailbox=%s" % entityMailbox)
-		space.teleportSpace(entityMailbox, position, direction)
+		self._spaceAllocs[spaceUType].teleportSpace(entityMailbox, position, direction)
