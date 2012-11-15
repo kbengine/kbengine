b14a477c2abfc8c8ff6836532fe93728a2808aae
kbengine <liquidx@163.com>
2012-11-15 11:27:41 +0800

    完善逻辑

--- demo/res/scripts/server_common/wtimer.py
+++ demo/res/scripts/server_common/wtimer.py
@@ -9,3 +9,4 @@ TIMER_TYPE_CREATE_SPACES							= 3 # 创建space
 TIMER_TYPE_DESTROY									= 4 # 延时销毁entity
 TIMER_TYPE_HEARDBEAT								= 5	# 心跳
 TIMER_TYPE_FIGTH_WATI_INPUT_TIMEOUT					= 6	# 战斗回合等待用户输入超时
+TIMER_TYPE_SPAWN									= 7	# 出生点出生timer
