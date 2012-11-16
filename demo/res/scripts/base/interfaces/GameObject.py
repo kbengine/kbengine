e13b7d49e9c881c2149ec86d8ac819a821b33d49
kbengine <liquidx@163.com>
2012-11-16 09:37:52 +0800

    继续完善逻辑部分

--- demo/res/scripts/base/interfaces/GameObject.py
+++ demo/res/scripts/base/interfaces/GameObject.py
@@ -13,4 +13,24 @@ class GameObject(KBEngine.Base):
 		#DEBUG_MSG("%s::onTimer: %i, tid:%i, arg:%i" % (self.getScriptName(), self.id, tid, userArg))
 		self._timermap[userArg](self, tid, userArg)
 
+	def destroySelf(self):
+		"""
+		virtual method
+		"""
+		if self.cell is not None:
+			# 销毁cell实体
+			self.destroyCellEntity()
+			return
+			
+		# 销毁base
+		self.destroy()
+
+	def onLoseCell(self):
+		"""
+		KBEngine method.
+		entity的cell部分实体丢失
+		"""
+		DEBUG_MSG("%s::onLoseCell: %i" % (self.getScriptName(), self.id))
+		self.destroySelf()
+		
 GameObject._timermap = {}
