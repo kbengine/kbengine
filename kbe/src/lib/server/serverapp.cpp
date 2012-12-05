96ba3cf6e4027723aaa04267246d1090a233d0af
kbengine <liquidx@163.com>
2012-12-05 17:22:04 +0800

    fixed:serverapp.cpp:68: error: 'max' was not declared in this scope

--- kbe/src/lib/server/serverapp.cpp
+++ kbe/src/lib/server/serverapp.cpp
@@ -65,7 +65,7 @@ threadPool_()
 	networkInterface_.pChannelTimeOutHandler(this);
 	networkInterface_.pChannelDeregisterHandler(this);
 
-	startActiveTick(max(1.f, Mercury::g_channelInternalTimeout / 2.0f));
+	startActiveTick(KBE_MIN(1.f, Mercury::g_channelInternalTimeout / 2.0f));
 }
 
 //-------------------------------------------------------------------------------------
