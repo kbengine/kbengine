402a92fbc3af8882206da563f2238db97e60857c
kbengine <liquidx@163.com>
2012-12-26 14:54:02 +0800

    added profile.

--- kbe/src/examples/testnetwork/main.cpp
+++ kbe/src/examples/testnetwork/main.cpp
@@ -459,6 +459,7 @@ void init_network(void)
 		packet2 >> accountname;
 		packet2 >> ip;
 		packet2 >> iport;
+		packet2.readBlob(bindatas);
 		printf("Client::onLoginSuccessfully: 获取返回的网关ip地址 size(%d) msgID=%u, ip:%s, port=%u.\n", len, msgID, ip.c_str(), iport);
 
 		// 连接网关
