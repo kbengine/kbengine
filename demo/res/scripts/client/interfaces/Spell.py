6a518fde447d81121230b1b72256edc8f1b1f218
kbengine <liquidx@163.com>
2013-01-12 18:05:47 +0800

    fixed:   File "D:\kbengine\demo\res/scripts/bots\Avatar.py", line 11, in <module>     from interfaces.Spell import Spell SCRIPT bots [2013-01-12 18:04:26,153] -   File "D:\kbengine\demo\res/scripts/bots\interfaces\Spell.py", line 5, in <module>     import wtimer SCRIPT bots [2013-01-12 18:04:26,153] - ImportError: No module named wtimer

--- demo/res/scripts/client/interfaces/Spell.py
+++ demo/res/scripts/client/interfaces/Spell.py
@@ -2,12 +2,10 @@
 import KBEngine
 import skills
 import GlobalConst
-import wtimer
 from KBEDebug import * 
 
 class Spell:
 	def __init__(self):
-		#self.addTimer(1,1,wtimer.TIMER_TYPE_BUFF_TICK)
 		pass
 		
 	def addDBuff(self, buffData):
@@ -32,30 +30,8 @@ class Spell:
 		
 	def spellTarget(self, srcEntityID, skillID, targetID):
 		"""
-		exposed.
 		对一个目标entity施放一个技能
 		"""
-		if srcEntityID != self.id:
-			return
-			 
-		DEBUG_MSG("Spell::spellTarget(%i):skillID=%i, srcEntityID=%i, targetID=%i" % (self.id, skillID, srcEntityID, targetID))
-		
-		skill = skills.getSkill(skillID)
-		if skill is None:
-			ERROR_MSG("Spell::spellTarget(%i):skillID=%i not found" % (self.id, skillID))
-			return
-
-		target = KBEngine.entities.get(targetID)
-		if target is None:
-			ERROR_MSG("Spell::spellTarget(%i):targetID=%i not found" % (self.id, targetID))
-			return
-
-		ret = skill.canUse(self, target)
-		if ret != GlobalConst.GC_OK:
-			ERROR_MSG("Spell::spellTarget(%i): cannot spell skillID=%i, targetID=%i, code=%i" % (self.id, skillID, targetID, ret))
-			return
-			
-		skill.use(self, target)
+		pass
 
 Spell._timermap = {}
-Spell._timermap[wtimer.TIMER_TYPE_BUFF_TICK] = Spell.onBuffTick
