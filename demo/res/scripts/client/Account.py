e3f7d80f5879d4d6265df859a724c50cb477c972
kbengine <liquidx@163.com>
2013-01-16 17:42:12 +0800

    fixed:AttributeError: 'Account' object has no attribute 'accountName'

--- demo/res/scripts/client/Account.py
+++ demo/res/scripts/client/Account.py
@@ -5,7 +5,7 @@ from KBEDebug import *
 class Account(KBEngine.Entity):
 	def __init__(self):
 		KBEngine.Entity.__init__(self)
-		DEBUG_MSG("my account name=%s." % self.accountName)
+		DEBUG_MSG("Account::__init__:%s." % (self.__dict__))
 
 	def onReqAvatarList(self, infos):
 		"""
