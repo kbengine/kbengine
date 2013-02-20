c41fc755cb207486b368450ece7e69c8213b0d02
kbengine <liquidx@163.com>
2013-02-20 11:25:10 +0800

    added memory_helper.hpp

---
+++ kbe/src/lib/helper/memory_helper.hpp
@@ -0,0 +1,39 @@
+/*
+This source file is part of KBEngine
+For the latest info, see http://www.kbengine.org/
+
+Copyright (c) 2008-2012 KBEngine.
+
+KBEngine is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+KBEngine is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+ 
+You should have received a copy of the GNU Lesser General Public License
+along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef __KBE_MEMORY_HELPER_HPP__
+#define __KBE_MEMORY_HELPER_HPP__
+
+#include "cstdkbe/cstdkbe.hpp"
+
+#if KBE_PLATFORM == PLATFORM_WIN32
+#ifdef _DEBUG
+#include "vld/vld.h"
+#endif
+#endif
+
+namespace KBEngine{
+
+
+
+}
+
+#endif // __KBE_DEBUG_OPTION_HPP__
