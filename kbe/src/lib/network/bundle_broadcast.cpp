7e5eaa61522d124a37963199341c9078f34167b2
kbengine <liquidx@163.com>
2013-04-08 15:03:12 +0800

    增加debug信息

--- kbe/src/lib/network/bundle_broadcast.cpp
+++ kbe/src/lib/network/bundle_broadcast.cpp
@@ -158,8 +158,9 @@ bool BundleBroadcast::receive(MessageArgs* recvArgs, sockaddr_in* psin, int32 ti
 			{
 				if(showerr)
 				{
-					ERROR_MSG("BundleBroadcast::receive: is failed!\n");
+					ERROR_MSG("BundleBroadcast::receive: is failed(please check {firewall rule | broadcastaddr is LAN})!\n");
 				}
+				
 				return false;
 			}
 			else
