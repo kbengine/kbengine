af9be96d9c4003798f7a97b4f7b27e7458744d99
kbengine <liquidx@163.com>
2013-05-03 11:57:30 +0800

    fixed: clientapp.cpp: In member function 'virtual bool KBEngine::ClientApp::installPyModules()': clientapp.cpp:221: error: 'log4cxx' has not been declared clientapp.cpp:226: error: 'log4cxx' has not been declared clientapp.cpp:231: error: 'log4cxx' has not been declared clientapp.cpp:236: error: 'log4cxx' has not been declared clientapp.cpp:241: error: 'log4cxx' has not been declared

--- kbe/src/lib/client_lib/clientapp.cpp
+++ kbe/src/lib/client_lib/clientapp.cpp
@@ -24,6 +24,7 @@ along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
 #include "entity.hpp"
 #include "config.hpp"
 #include "cstdkbe/kbeversion.hpp"
+#include "helper/script_loglevel.hpp"
 #include "network/channel.hpp"
 #include "network/tcp_packet_receiver.hpp"
 #include "thread/threadpool.hpp"
