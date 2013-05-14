a6475553b6197a134bd11ceabbd5907fdfe0ecbe
kbengine <liquidx@163.com>
2013-05-14 16:14:17 +0800

    完善战斗脚本

--- demo/res/scripts/client/interfaces/Combat.py
+++ demo/res/scripts/client/interfaces/Combat.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import KBEngine
 from KBEDebug import * 
+from interfaces.CombatPropertys import CombatPropertys
 
-class Combat:
+class Combat(CombatPropertys):
 	def __init__(self):
-		pass
+		CombatPropertys.__init__(self)
 
 	def recvDamage(self, attackerID, skillID, damageType, damage):
 		"""
@@ -13,5 +14,7 @@ class Combat:
 		DEBUG_MSG("%s::recvDamage: %i attackerID=%i, skillID=%i, damageType=%i, damage=%i" % \
 			(self.getScriptName(), self.id, attackerID, skillID, damageType, damage))
 		
+		# 通知表现层改变表现
+		KBEngine.fireEvent("recvDamage", self.id, attackerID, skillID, damageType, damage)
 		
-Combat._timermap = {}
+
