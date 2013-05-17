147c01065f6612f29eb3a3afa82e95b793c474a8
kbengine <liquidx@163.com>
2013-05-17 16:49:01 +0800

    普通攻击ID为1

--- demo/res/scripts/data/d_skills.py
+++ demo/res/scripts/data/d_skills.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-datas={10011001: {'name': '普通攻击', 'descr': '', 'script': 'SkillAttack', 'HP': 0, 'MP': 0, 'speed': 0.0, 'id': 10011001, 'icon': 0}}
+datas={1: {'name': '普通攻击', 'descr': '', 'script': 'SkillAttack', 'HP': 0, 'MP': 0, 'speed': 0.0, 'id': 1, 'icon': 0}}
 effsdatas={}
