58609c3b6db38ee70ef2d95ce1a38ee8ea46822b
kbengine <liquidx@163.com>
2013-07-01 14:39:46 +0800

    added Poller.

---
+++ demo/res/scripts/base/Poller.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import KBEngine
+import Functor
+import socket
+from KBEDebug import *
+
+class Poller:
+	"""
+	"""
+	def __init__(self):
+		self._socket = None
+		self._clients = {}
+		
+	def startServer(self, addr, port):
+		"""
+		virtual method.
+		"""
+		self._socket = socket.socket()
+		self._socket.bind((addr, port))
+		self._socket.listen(10)
+		
+		KBEngine.registerFileDescriptor(self._socket.fileno(), self.onRecv)
+		# KBEngine.registerWriteFileDescriptor(self._socket.fileno(), self.onWrite)
+
+	def close(self):
+		if self._socket:
+			KBEngine.deregisterFileDescriptor(self._socket.fileno())
+			self._socket.close()
+			self._socket = None
+		
+	def onWrite(self, fileno):
+		pass
+		
+	def onRecv(self, fileno):
+		if self._socket.fileno() == fileno:
+			sock, addr = self._socket.accept()
+			self._clients[sock.fileno()] = (sock, addr)
+			KBEngine.registerFileDescriptor(sock.fileno(), self.onRecv)
+			DEBUG_MSG("Poller::onRecv: new channel[%s/%i]" % (addr, sock.fileno()))
+		else:
+			sock, addr = self._clients.get(fileno, None)
+			if sock is None:
+				return
+			
+			data = sock.recv(2048)
+			DEBUG_MSG("Poller::onRecv: %s/%i get data, size=%i" % (addr, sock.fileno(), len(data)))
+			self.processData(sock, data)
+			KBEngine.deregisterFileDescriptor(sock.fileno())
+			sock.close()
+			del self._clients[fileno]
+			
+	def processData(self, sock, datas):
+		"""
+		处理接收数据
+		"""
+		pass
