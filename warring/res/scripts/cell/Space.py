c91946170d1c92f090a243389641d25d61270718
kbengine <liquidx@163.com>
2013-10-12 16:18:51 +0800

    修正获取场景配置出错的bug

--- warring/res/scripts/cell/Space.py
+++ warring/res/scripts/cell/Space.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import KBEngine
+import d_spaces
 from KBEDebug import *
 from interfaces.GameObject import GameObject
 
 class Space(GameObject):
 	def __init__(self):
 		GameObject.__init__(self)
-		DEBUG_MSG('created space[%d] entityID = %i.' % (self.spaceUType, self.id))
+		DEBUG_MSG('created space[%d] res=%s, entityID = %i.' % (self.spaceUType, d_spaces.datas[self.spaceUType].get("resPath", ""), self.id))
 		
-		KBEngine.addSpaceGeometryMapping(self.spaceID, None, "../../res/Media/Scenes/Scene1")
+		KBEngine.addSpaceGeometryMapping(self.spaceID, None, d_spaces.datas[self.spaceUType].get("resPath", ""))
 		KBEngine.globalData["space_%i" % self.spaceID] = self.base
 	
 	def onDestroy(self):
