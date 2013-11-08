409f719448f2415b5759ddcb575be13af8c9309b
kbengine <liquidx@163.com>
2013-11-08 10:45:59 +0800

    更新逻辑

--- warring/res/scripts/base/Space.py
+++ warring/res/scripts/base/Space.py
@@ -38,7 +38,7 @@ class Space(GameObject):
 			scale = int(((float(scaleNode[0].text) + float(scaleNode[1].text) + float(scaleNode[2].text)) / 3.0) * 10)
 			self.tmpCreateEntityDatas.append([int(child.attrib['name']), \
 			(float(position[0].text), float(position[1].text), float(position[2].text)), \
-			(float(direction[0].text) * (math.pi / 128), float(direction[1].text) * (math.pi / 128), float(direction[2].text) * (math.pi / 128)), \
+			(float(direction[0].text) * ((math.pi * 2) / 360), float(direction[1].text) * ((math.pi * 2) / 360), float(direction[2].text) * ((math.pi * 2) / 360)), \
 			scale, \
 			])
 		
