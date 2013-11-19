f41e1547c34cce71fe10330d357bca9b93dac486
kbengine <liquidx@163.com>
2013-11-19 15:53:57 +0800

    修改逻辑

--- warring/res/scripts/cell/interfaces/Motion.py
+++ warring/res/scripts/cell/interfaces/Motion.py
@@ -43,7 +43,7 @@ class Motion:
 		使用引擎的任何移动相关接口， 在entity移动结束时均会调用此接口
 		"""
 		self.isMoving = False
-		
+
 	def randomWalk(self, basePos):
 		"""
 		随机移动entity
@@ -53,15 +53,23 @@ class Motion:
 			
 		if time.time() < self.nextMoveTime:
 			return False
+		
+		while True:
+			rnd = random.random()
+			a = 30.0 * rnd				# 移动半径距离在30米内
+			b = 360.0 * rnd				# 随机一个角度
+			x = a * math.cos( b ) 		# 半径 * 正余玄
+			z = a * math.sin( b )
 			
-		rnd = random.random()
-		a = 30.0 * rnd				# 移动半径距离在30米内
-		b = 360.0 * rnd				# 随机一个角度
-		x = a * math.cos( b ) 		# 半径 * 正余玄
-		z = a * math.sin( b )
-		self.gotoPosition((basePos.x + x, basePos.y, basePos.z + z))
-		self.isMoving = True
-		self.nextMoveTime = int(time.time() + random.randint(5, 15))
+			destPos = (basePos.x + x, basePos.y, basePos.z + z)
+			
+			if self.position.distTo(destPos) < 2.0:
+				continue
+				
+			self.gotoPosition(destPos)
+			self.isMoving = True
+			self.nextMoveTime = int(time.time() + random.randint(5, 15))
+			break
 
 	def resetSpeed(self):
 		walkSpeed = self.getDatas()["moveSpeed"]
@@ -93,7 +101,7 @@ class Motion:
 			
 		if entity.position.distTo(self.position) <= dist:
 			return
-			
+
 		self.isMoving = True
 		self.moveToEntity(targetID, self.moveSpeed * 0.1, dist, 1, True, True)
 		
@@ -107,7 +115,7 @@ class Motion:
 
 		if self.position.distTo(position) <= 0.05:
 			return
-			
+
 		self.isMoving = True
 		self.moveToPoint(tuple(position), self.moveSpeed * 0.1, 1, True, True)
 		
