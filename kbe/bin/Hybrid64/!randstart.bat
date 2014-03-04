68276c2b8ca2d6b5fb0c32b8468c10246257bff5
kbengine <admin@kbengine.org>
2014-03-04 16:31:41 +0800

    up

---
+++ kbe/bin/Hybrid64/!randstart.bat
@@ -0,0 +1,27 @@
+@echo off
+set curpath=%~dp0
+set KBE_ROOT=%curpath:~0,-15%
+set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%demo/;%KBE_ROOT%demo/res/
+set KBE_HYBRID_PATH=%KBE_ROOT%kbe/bin/Hybrid64/
+
+if defined uid (echo %uid%) else set uid=%random%%%32760+1
+
+start kbmachine.exe
+#ping 127.0.0.1 -n 1
+start billingsystem.exe
+#ping 127.0.0.1 -n 1
+#start messagelog.exe
+#ping 127.0.0.1 -n 1
+#start resourcemgr.exe
+ping 127.0.0.1 -n 1
+start dbmgr.exe
+ping 127.0.0.1 -n 2
+start baseappmgr.exe
+ping 127.0.0.1 -n 1
+start cellappmgr.exe
+ping 127.0.0.1 -n 1
+start baseapp.exe
+ping 127.0.0.1 -n 1
+start cellapp.exe
+ping 127.0.0.1 -n 1
+start loginapp.exe
