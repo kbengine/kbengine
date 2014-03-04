b4f784614e96ac0d8e9eeb9d7e0d0481484c0806
kbengine <admin@kbengine.org>
2014-03-04 16:43:08 +0800

    up

--- kbe/bin/Hybrid/!(win)start_bots.bat
+++ kbe/bin/Hybrid/!(win)start_bots.bat
@@ -1,7 +1,7 @@
 @echo off
 set curpath=%~dp0
 set KBE_ROOT=%curpath:~0,-15%
-set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%warring/;%KBE_ROOT%warring/res/
+set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%demo/;%KBE_ROOT%demo/res/
 set KBE_HYBRID_PATH=%KBE_ROOT%kbe/bin/Hybrid/
 
 
