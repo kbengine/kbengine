ed7af2696b777e9b721e673f496e065cf590eb06
kbengine <admin@kbengine.org>
2014-04-04 18:17:46 +0800

    修改提示信息.

--- kbe/src/server/baseapp/base.cpp
+++ kbe/src/server/baseapp/base.cpp
@@ -168,7 +168,7 @@ void Base::createCellData(void)
 		{
 			if(!scriptModule_->hasCell())
 			{
-				WARNING_MSG(boost::format("%1%::createCellData: cellData no create, cellapp not found[%2%].\n") % 
+				WARNING_MSG(boost::format("%1%::createCellData: cellData no create, not from the cellapp found script(%2%)!\n") % 
 					scriptModule_->getName() % scriptModule_->getName());
 			}
 		}
