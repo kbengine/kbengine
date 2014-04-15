739cf94056c1fc9b445535959c5332507751f2e5
kbengine <admin@kbengine.org>
2014-04-15 18:52:16 +0800

    up

--- kbe/tools/server/guiconsole/!(win)start.bat
+++ kbe/tools/server/guiconsole/!(win)start.bat
@@ -1,8 +1,8 @@
 @echo off
 set curpath=%~dp0
-if defined uid (echo %KBE_ROOT%) else set KBE_ROOT=%curpath:~0,-28%
-if defined uid (echo %KBE_RES_PATH%) else set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%demo/;%KBE_ROOT%demo/res/
-if defined uid (echo %KBE_HYBRID_PATH%) else set KBE_HYBRID_PATH=%KBE_ROOT%kbe/bin/Hybrid/
+if defined KBE_ROOT (echo %KBE_ROOT%) else set KBE_ROOT=%curpath:~0,-28%
+if defined KBE_RES_PATH (echo %KBE_RES_PATH%) else set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%demo/;%KBE_ROOT%demo/res/
+if defined KBE_HYBRID_PATH (echo %KBE_HYBRID_PATH%) else set KBE_HYBRID_PATH=%KBE_ROOT%kbe/bin/Hybrid/
 
 if defined uid (echo %uid%) else set uid=%random%%%32760+1
 
