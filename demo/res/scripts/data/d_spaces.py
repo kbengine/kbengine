680c725fe51959b11be39a7bc66b3d4940a48c58
kbengine <liquidx@163.com>
2014-06-17 13:14:15 +0800

    up

--- demo/res/scripts/data/d_spaces.py
+++ demo/res/scripts/data/d_spaces.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-datas={1: {'entities': (10001001, 10002001, 10003001), 'entityType': 'Space', 'resPath': 'xinshoucun', 'id': 1, 'name': '新手村', 'type': 1}, 2: {'entities': (20001001, 20002001, 20003001), 'entityType': 'Space', 'resPath': 'ogredemo', 'id': 2, 'name': 'OgreDemo', 'type': 1}, 3: {'entities': (), 'entityType': 'Space', 'resPath': 'minidemo', 'id': 3, 'name': 'minidemo', 'type': 1}}
+datas={1: {'resPath': 'xinshoucun', 'id': 1, 'name': '新手村', 'type': 1, 'entities': (10001001, 10002001, 10003001), 'entityType': 'Space'}, 2: {'resPath': 'OgreDemo', 'id': 2, 'name': 'ogredemo', 'type': 1, 'entities': (20001001, 20002001, 20003001), 'entityType': 'Space'}, 3: {'resPath': 'minidemo', 'id': 3, 'name': 'minidemo', 'type': 1, 'entities': (), 'entityType': 'Space'}, 4: {'resPath': 'teleportspace', 'id': 4, 'name': 'teleportspace', 'type': 1, 'entities': (), 'entityType': 'Space'}}
 
 allDatas = {
 	'场景表':datas,
