8866d2dba9f554195e58fec53db5908ee6451e52
kbengine <liquidx@163.com>
2014-11-15 17:47:01 +0800

    增加注释

--- kbe/src/lib/db_mysql/common.hpp
+++ kbe/src/lib/db_mysql/common.hpp
@@ -56,6 +56,17 @@ struct DB_OP_TABLE_ITEM_DATA
 	std::string extraDatas;
 };
 
+/**
+	描述一个实体某张表的数据，读和写都会使用到
+	dbids：主表上dbids中只有一个dbid，就是实体的id，如果实体数据存在数组类则会有子表出现，当这个数据结构描述的是一个子表的时候
+	dbids是这个数组的子表索引, 每个dbid都表示这个子表上对应的值并且按照排列顺序同时也表示在数组中对应位置的值。
+
+	items：中有这个表的字段信息，如果是写库则字段中也有对应的要写值。
+
+	optable：子表结构
+
+	results：读操作时查询到的数据, 数据的排列对应items中的strkey的数量乘以dbids的数量。
+*/
 struct DB_OP_TABLE_ITEM_DATA_BOX
 {
 	DB_OP_TABLE_ITEM_DATAS items;
