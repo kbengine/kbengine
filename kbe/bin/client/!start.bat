60719305fc07ddc9aad25703346b06ca318a9fe1
kbengine <liquidx@163.com>
2014-11-21 12:12:23 +0800

    不需要这个文件夹了

--- kbe/bin/client/!start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set curpath=%~dp0
-set KBE_ROOT=%curpath:~0,-15%
-set KBE_RES_PATH=%KBE_ROOT%kbe/res/;%KBE_ROOT%demo/;%KBE_ROOT%demo/res/
-set KBE_BIN_PATH=%KBE_ROOT%kbe/bin/server/
-
-start client_d
