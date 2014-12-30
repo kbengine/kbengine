bcdcd502163304c760e25d9aa83852e678870ab7
kbengine <liquidx@163.com>
2014-12-30 18:02:17 +0800

    up

--- assets/start_server_rand.sh
+++ assets/start_server_rand.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export KBE_ROOT="../"
-export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$KBE_ROOT/demo:$KBE_ROOT/demo/res/:$KBE_ROOT/demo/scripts/"
+export KBE_ROOT=$(cd ../; pwd)
+export KBE_RES_PATH="$KBE_ROOT/kbe/res/:$KBE_ROOT/assets:$KBE_ROOT/assets/res/:$KBE_ROOT/assets/scripts/"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server/"
 
 echo KBE_ROOT = \"${KBE_ROOT}\"
