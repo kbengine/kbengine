dbc2c92c36f56370d2d0719d35fd783c931ae9d6
kbengine <liquidx@163.com>
2015-02-04 17:08:24 +0800

    up

--- assets/start_server_fixed.sh
+++ assets/start_server_fixed.sh
@@ -10,13 +10,13 @@ echo KBE_BIN_PATH = \"${KBE_BIN_PATH}\"
 
 sh ./kill_server.sh
 
-$KBE_BIN_PATH/machine --cid=2129652375332859700 --grouporder=1  --globalorder=1&
-$KBE_BIN_PATH/logger --cid=1129653375331859700 --grouporder=1 --globalorder=2&
-$KBE_BIN_PATH/interfaces --cid=1129652375332859700 --grouporder=1 --globalorder=3&
-$KBE_BIN_PATH/dbmgr --cid=3129652375332859700 --grouporder=1 --globalorder=4&
-$KBE_BIN_PATH/baseappmgr --cid=4129652375332859700 --grouporder=1  --globalorder=5&
-$KBE_BIN_PATH/cellappmgr --cid=5129652375332859700 --grouporder=1  --globalorder=6&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859700 --grouporder=1  --globalorder=7&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859700 --grouporder=1  --globalorder=8&
-$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --grouporder=1  --globalorder=9&
+$KBE_BIN_PATH/machine --cid=2129652375332859700 --gus=1&
+$KBE_BIN_PATH/logger --cid=1129653375331859700 --gus=2&
+$KBE_BIN_PATH/interfaces --cid=1129652375332859700 --gus=3&
+$KBE_BIN_PATH/dbmgr --cid=3129652375332859700 --gus=4&
+$KBE_BIN_PATH/baseappmgr --cid=4129652375332859700 --gus=5&
+$KBE_BIN_PATH/cellappmgr --cid=5129652375332859700 --gus=6&
+$KBE_BIN_PATH/baseapp --cid=6129652375332859700 --gus=7&
+$KBE_BIN_PATH/cellapp --cid=7129652375332859700 --gus=8&
+$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --gus=9&
 
