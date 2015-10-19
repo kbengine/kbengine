fb9eb1b67ed3c36207e0ce68dd00b83fbe9c3640
dieswolf <dieswolf@gmail.com>
2015-10-19 14:08:07 +0800

    修正linux下的编译错误

--- kbe/src/server/tools/interfaces/interfaces.h
+++ kbe/src/server/tools/interfaces/interfaces.h
@@ -121,8 +121,8 @@ public:
 
 	/** Timer²Ù×÷
 	*/
-	static PyObject* Interfaces::__py_addTimer(float interval, float repeat, PyObject *callback);
-	static PyObject* Interfaces::__py_delTimer(ScriptID timerID);
+	static PyObject* __py_addTimer(float interval, float repeat, PyObject *callback);
+	static PyObject* __py_delTimer(ScriptID timerID);
 
 	typedef KBEUnordered_map<std::string, KBEShared_ptr<Orders> > ORDERS;
 	Interfaces::ORDERS& orders(){ return orders_; }
