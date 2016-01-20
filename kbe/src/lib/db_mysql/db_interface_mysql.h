3dad61693982835ae0ad1155f840d661acad6a97
kbengine <liquidx@163.com>
2016-01-20 14:35:24 +0800

    https://github.com/kbengine/kbengine/issues/282 windows x64编译支持

--- kbe/src/lib/db_mysql/db_interface_mysql.h
+++ kbe/src/lib/db_mysql/db_interface_mysql.h
@@ -32,11 +32,9 @@ along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
 #include "mysql/mysql.h"
 #if KBE_PLATFORM == PLATFORM_WIN32
 #ifdef X64
-#pragma comment (lib, "yassl64.lib")
 #pragma comment (lib, "libmysql64.lib")
 #pragma comment (lib, "mysqlclient64.lib")
 #else
-#pragma comment (lib, "yassl32.lib")
 #pragma comment (lib, "libmysql32.lib")
 #pragma comment (lib, "mysqlclient32.lib")
 #endif
