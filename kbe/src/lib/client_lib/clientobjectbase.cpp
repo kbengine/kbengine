54fc179d0e178508e04bb4e57c916e00fd887ec8
kbengine <admin@kbengine.org>
2016-04-09 12:51:15 +0800

    up

--- kbe/src/lib/client_lib/clientobjectbase.cpp
+++ kbe/src/lib/client_lib/clientobjectbase.cpp
@@ -396,18 +396,8 @@ ENTITY_ID ClientObjectBase::getAoiEntityID(ENTITY_ID id)
 ENTITY_ID ClientObjectBase::getAoiEntityIDFromStream(MemoryStream& s)
 {
 	ENTITY_ID id = 0;
-
-	if (!EntityDef::entityAliasID())
-	{
-		s >> id;
-		return id;
-	}
-
-	if(pEntityIDAliasIDList_.size() > 255)
-	{
-		s >> id;
-	}
-	else
+	if(EntityDef::entityAliasID() && 
+		pEntityIDAliasIDList_.size() > 0 && pEntityIDAliasIDList_.size() <= 255)
 	{
 		uint8 aliasID = 0;
 		s >> aliasID;
@@ -415,10 +405,20 @@ ENTITY_ID ClientObjectBase::getAoiEntityIDFromStream(MemoryStream& s)
 		// 如果为0且客户端上一步是重登陆或者重连操作并且服务端entity在断线期间一直处于在线状态
 		// 则可以忽略这个错误, 因为cellapp可能一直在向baseapp发送同步消息， 当客户端重连上时未等
 		// 服务端初始化步骤开始则收到同步信息, 此时这里就会出错。
-		if (pEntityIDAliasIDList_.size() <= aliasID)
+		if(pEntityIDAliasIDList_.size() == 0)
 			return 0;
 
 		id = pEntityIDAliasIDList_[aliasID];
+	}
+	else
+	{
+		// 如果为0且客户端上一步是重登陆或者重连操作并且服务端entity在断线期间一直处于在线状态
+		// 则可以忽略这个错误, 因为cellapp可能一直在向baseapp发送同步消息， 当客户端重连上时未等
+		// 服务端初始化步骤开始则收到同步信息, 此时这里就会出错。
+		if(pEntityIDAliasIDList_.size() == 0)
+			return 0;
+
+		s >> id;
 	}
 
 	return id;
@@ -1217,19 +1217,6 @@ void ClientObjectBase::onUpdateBasePosXZ(Network::Channel* pChannel, MemoryStrea
 }
 
 //-------------------------------------------------------------------------------------
-void ClientObjectBase::onUpdateBaseDir(Network::Channel* pChannel, MemoryStream& s)
-{
-	float yaw, pitch, roll;
-	s >> yaw >> pitch >> roll;
-
-	client::Entity* pEntity = pPlayer();
-	if (pEntity)
-	{
-		// @TODO(phw)：这里将来需要与controlledBy机制一起实现
-	}
-}
-
-//-------------------------------------------------------------------------------------
 void ClientObjectBase::onSetEntityPosAndDir(Network::Channel* pChannel, MemoryStream& s)
 {
 	ENTITY_ID eid;
@@ -1787,11 +1774,6 @@ void ClientObjectBase::onStreamDataRecv(Network::Channel* pChannel, MemoryStream
 
 //-------------------------------------------------------------------------------------
 void ClientObjectBase::onStreamDataCompleted(Network::Channel* pChannel, int16 id)
-{
-}
-
-//-------------------------------------------------------------------------------------
-void ClientObjectBase::onControlEntity(Network::Channel* pChannel, int32 entityID, int8 isControlled)
 {
 }
 
