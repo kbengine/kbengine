68d8f273eebfae60a5bbec37bd91a608812ae2a2
wuhaowei <rmdevilmay@gmail.com>
2016-08-22 16:22:59 +0800

    增加： 1、增加实时日志和日志筛选功能； 2、服务器管理增加关闭某个类型组件的功能； 3、“加载服务器配置”页面增加Component、Type、cid、gus、ip四个参数显示。 修改： 1、修改“停止服务器”和“加载服务器配置”页面为弹出窗口形式； 2、修改python控制台用户输入命令字体颜色。

--- kbe/tools/server/webconsole/WebConsole/views.py
+++ kbe/tools/server/webconsole/WebConsole/views.py
@@ -54,6 +54,45 @@ def components_query( request ):
 	
 	return HttpResponse( json.dumps( kbeComps ), content_type="application/json" )
 
+def components_group_query( request , ct):
+	"""
+	请求获取一组组件数据
+	"""
+	ct = int(ct)
+	components = Machines.Machines( request.session["sys_uid"], request.session["sys_user"] )
+	components.queryAllInterfaces(timeout = 0.5)
+
+	# [ [machine, other-components, ...], ...]
+	kbeComps = []
+	for mID, comps in components.interfaces_groups.items():
+		if len( comps ) <= 1:
+			continue
+
+		dl = []
+		kbeComps.append( dl )
+		for comp in comps:
+			if comp.componentType == ct or comp.componentType == 8:
+				d = {
+					"ip"            : comp.intaddr,
+					"componentType" : comp.componentType,
+					"componentName" : comp.componentName,
+					"fullname"      : comp.fullname,
+					"uid"           : comp.uid,
+					"pid"           : comp.pid,
+					"componentID"   : comp.componentID,
+					"globalOrderID" : comp.globalOrderID,
+					"cpu"           : comp.cpu,
+					"mem"           : comp.mem,
+					"usedmem"       : comp.usedmem,
+					"entities"      : comp.entities,
+					"proxies"       : comp.proxies,
+					"clients"       : comp.clients,
+					"consolePort"   : comp.consolePort,
+				}
+				dl.append( d )
+		
+	return HttpResponse( json.dumps( kbeComps ), content_type="application/json" )
+
 @login_check
 def components_query_machines( request ):
 	"""
@@ -145,6 +184,22 @@ def components_stop( request ):
 	# 当前Machine不支持停止单个组件
 
 @login_check
+def components_group_shutdown( request ,ct ):
+	"""
+	停止一组服务器组件
+	"""
+	ct = int(ct)
+	
+	components = Machines.Machines( request.session["sys_uid"], request.session["sys_user"] )
+	
+	components.stopServer( ct, trycount = 0 )
+	context = {
+		"shutType": "group_ct",
+		"ct":ct
+ 	}
+	return render( request, "WebConsole/components_shutdown.html", context )
+
+@login_check
 def components_shutdown( request ):
 	"""
 	停止服务器
@@ -165,8 +220,10 @@ def components_shutdown( request ):
 	
 	for ctid in COMPS_FOR_SHUTDOWN:
 		components.stopServer( ctid, trycount = 0 )
-	
-	return render( request, "WebConsole/components_shutdown.html", {} )
+	context = {
+		"shutType": "all_ct"
+ 	}
+	return render( request, "WebConsole/components_shutdown.html", context )
 
 @login_check
 def components_save_layout( request ):
