be44aa7623f7f8127af5ba22062563133f2728cd
dieswolf <dieswolf@gmail.com>
2016-11-15 08:01:10 +0800

    up

--- kbe/src/server/cellapp/witness.cpp
+++ kbe/src/server/cellapp/witness.cpp
@@ -736,7 +736,7 @@ bool Witness::update()
 	if(!pChannel)
 		return true;
 
-	if (aoiEntities_map_.size() > 0 || pEntity_->isControlledNotSelfCleint())
+	if (aoiEntities_map_.size() > 0 || pEntity_->isControlledNotSelfClient())
 	{
 		Network::Bundle* pSendBundle = pChannel->createSendBundle();
 		
