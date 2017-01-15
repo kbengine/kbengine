04586e888aeed1b61dd35f0d274d27cebf5a327d
kbengine <admin@kbengine.org>
2017-01-15 11:55:07 +0800

    up

--- kbe/src/server/baseapp/archiver.cpp
+++ kbe/src/server/baseapp/archiver.cpp
@@ -39,7 +39,7 @@ Archiver::~Archiver()
 //-------------------------------------------------------------------------------------
 void Archiver::tick()
 {
-	int32 periodInTicks = secondsToTicks(ServerConfig::getSingleton().getBaseApp().archivePeriod, 0.f);
+	int32 periodInTicks = (int32)secondsToTicks(ServerConfig::getSingleton().getBaseApp().archivePeriod, 0.f);
 	if (periodInTicks == 0)
 		return;
 
