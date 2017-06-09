84b9d8312ca389f9b2b612a51cd259da5fe3f632
kbengine <3603661@qq.com>
2017-06-09 14:22:18 +0800

    解决某些时候加载持久化的数组顺序不对的问题

--- kbe/src/lib/db_mysql/read_entity_helper.h
+++ kbe/src/lib/db_mysql/read_entity_helper.h
@@ -86,7 +86,23 @@ public:
 				sval >> item_dbid;
 
 				// 将dbid记录到列表中，如果当前表还存在子表引用则会去子表查每一条与此dbid相关的记录
-				context.dbids[context.dbid].push_back(item_dbid);
+				std::vector<DBID>& itemDBIDs = context.dbids[context.dbid];
+				int fidx = -100;
+
+				if (itemDBIDs.size() > 0 && itemDBIDs[itemDBIDs.size() - 1] > item_dbid)
+				{
+					for (fidx = itemDBIDs.size() - 1; fidx > 0; --fidx)
+					{
+						if (itemDBIDs[fidx] < item_dbid)
+							break;
+					}
+
+					itemDBIDs.insert(itemDBIDs.begin() + fidx, item_dbid);
+				}
+				else
+				{
+					itemDBIDs.push_back(item_dbid);
+				}
 
 				// 如果这条记录除了dbid以外还存在其他数据，则将数据填充到结果集中
 				if(nfields > 1)
@@ -98,7 +114,10 @@ public:
 						std::string data;
 						data.assign(arow[i], lengths[i]);
 
-						context.results.push_back(data);
+						if (fidx != -100)
+							context.results.insert(context.results.begin() + context.results.size() - (itemDBIDs.size() - fidx - 1), data);
+						else
+							context.results.push_back(data);
 					}
 				}
 			}
@@ -174,8 +193,25 @@ public:
 				sval >> parentID;
 
 				// 将dbid记录到列表中，如果当前表还存在子表引用则会去子表查每一条与此dbid相关的记录
-				context.dbids[parentID].push_back(item_dbid);
-				t_parentTableDBIDs.push_back(item_dbid);
+				std::vector<DBID>& itemDBIDs = context.dbids[parentID];
+				int fidx = -100;
+
+				if (itemDBIDs.size() > 0 && itemDBIDs[itemDBIDs.size() - 1] > item_dbid)
+				{
+					for (fidx = itemDBIDs.size() - 1; fidx > 0; --fidx)
+					{
+						if (itemDBIDs[fidx] < item_dbid)
+							break;
+					}
+
+					itemDBIDs.insert(itemDBIDs.begin() + fidx, item_dbid);
+					t_parentTableDBIDs.insert(t_parentTableDBIDs.begin() + t_parentTableDBIDs.size() - (itemDBIDs.size() - fidx - 1), item_dbid);
+				}
+				else
+				{
+					itemDBIDs.push_back(item_dbid);
+					t_parentTableDBIDs.push_back(item_dbid);
+				}
 
 				// 如果这条记录除了dbid以外还存在其他数据，则将数据填充到结果集中
 				const uint32 const_fields = 2; // id, parentID
@@ -188,7 +224,10 @@ public:
 						std::string data;
 						data.assign(arow[i], lengths[i]);
 
-						context.results.push_back(data);
+						if (fidx != -100)
+							context.results.insert(context.results.begin() + context.results.size() - (itemDBIDs.size() - fidx - 1), data);
+						else
+							context.results.push_back(data);
 					}
 				}
 			}
