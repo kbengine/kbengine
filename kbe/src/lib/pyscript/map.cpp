d5fd868045b4d57fb170f12e39ad64495cc92d77
kbe <3603661@qq.com>
2017-10-23 14:14:51 +0800

    up

--- kbe/src/lib/pyscript/map.cpp
+++ kbe/src/lib/pyscript/map.cpp
@@ -27,7 +27,7 @@ along with KBEngine.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace KBEngine{ namespace script{
 
-/** python mapæ“ä½œæ‰€éœ€è¦çš„æ–¹æ³•è¡¨ */
+/** python map²Ù×÷ËùÐèÒªµÄ·½·¨±í */
 PyMappingMethods Map::mappingMethods =
 {
 	(lenfunc)Map::mp_length,					// mp_length
@@ -35,7 +35,7 @@ PyMappingMethods Map::mappingMethods =
 	(objobjargproc)Map::mp_ass_subscript		// mp_ass_subscript
 };
 
-// å‚è€ƒ objects/dictobject.c
+// ²Î¿¼ objects/dictobject.c
 // Hack to implement "key in dict"
 PySequenceMethods Map::mappingSequenceMethods = 
 {
@@ -172,9 +172,13 @@ PyObject* Map::__py_get(PyObject* self, PyObject* args)
 	if (!pyObj)
 	{
 		if (PySequence_Size(args) > 1)
+		{
 			return PySequence_GetItem(args, 1);
+		}
 		else
-			return NULL;
+		{
+			S_Return;
+		}
 	}
 	else
 	{
