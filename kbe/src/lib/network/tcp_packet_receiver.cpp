219b662283d603fd6e7aa4469cecdca7d2dfaf81
kebiao <380000937@qq.com>
2018-07-26 20:38:58 +0800

    up

--- kbe/src/lib/network/tcp_packet_receiver.cpp
+++ kbe/src/lib/network/tcp_packet_receiver.cpp
@@ -88,6 +88,11 @@ bool TCPPacketReceiver::processRecv(bool expectingPacket)
 
 		if(rstate == PacketReceiver::RECV_STATE_INTERRUPT)
 		{
+			if (pChannel->isInternal())
+			{
+				ERROR_MSG(fmt::format("TCPPacketReceiver::processRecv(): {}, error={}\n", pChannel->c_str(), kbe_lasterror()));
+			}
+
 			onGetError(pChannel);
 			return false;
 		}
@@ -96,6 +101,11 @@ bool TCPPacketReceiver::processRecv(bool expectingPacket)
 	}
 	else if(len == 0) // 客户端正常退出
 	{
+		if (pChannel->isInternal())
+		{
+			DEBUG_MSG(fmt::format("TCPPacketReceiver::processRecv(): disconnected! {}\n", pChannel->c_str()));
+		}
+
 		TCPPacket::reclaimPoolObject(pReceiveWindow);
 		onGetError(pChannel);
 		return false;
