d85b8abe7db4736d3409e5f2055cc5e63f0f34e2
kebiao <380000937@qq.com>
2018-09-13 15:25:52 +0800

    up

--- kbe/src/server/dbmgr/dbtasks.cpp
+++ kbe/src/server/dbmgr/dbtasks.cpp
@@ -1500,7 +1500,7 @@ bool DBTaskAccountLogin::db_thread_process()
 	// 如果Interfaces已经判断不成功就没必要继续下去
 	if(retcode_ != SERVER_SUCCESS)
 	{
-		ERROR_MSG(fmt::format("DBTaskAccountLogin::db_thread_process(): interfaces report failed({})!\n", retcode_));
+		ERROR_MSG(fmt::format("DBTaskAccountLogin::db_thread_process(): interfaces report failed(errcode={})!\n", retcode_));
 		return false;
 	}
 
