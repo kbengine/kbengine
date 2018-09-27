85559103655f2ee2601207e3e701b1b068915ed2
kebiao <380000937@qq.com>
2018-09-27 13:43:32 +0800

    up

--- kbe/src/lib/server/components.cpp
+++ kbe/src/lib/server/components.cpp
@@ -342,7 +342,7 @@ void Components::removeComponentByChannel(Network::Channel * pChannel, bool isSh
 				//SAFE_RELEASE((*iter).pExtAddr);
 				// (*iter).pChannel->decRef();
 
-				if (!isShutingdown && g_componentType != LOGGER_TYPE)
+				if (!isShutingdown && g_componentType != LOGGER_TYPE && g_componentType != INTERFACES_TYPE)
 				{
 					ERROR_MSG(fmt::format("Components::removeComponentByChannel: {} : {}, Abnormal exit! {}\n",
 						COMPONENT_NAME_EX(componentType), (*iter).cid, pChannel->condemnReason()));
