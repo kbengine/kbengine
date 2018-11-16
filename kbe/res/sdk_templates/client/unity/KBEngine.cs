0c79034273c7ad291db74fcf1b6dd9e1e42d8e8b
jay602 <857024709@qq.com>
2018-11-16 11:59:32 +0800

    登录失败log中除了错误码应该给出具体文字提示

--- kbe/res/sdk_templates/client/unity/KBEngine.cs
+++ kbe/res/sdk_templates/client/unity/KBEngine.cs
@@ -875,7 +875,7 @@
 		{
 			UInt16 failedcode = stream.readUint16();
 			_serverdatas = stream.readBlob();
-			Dbg.ERROR_MSG("KBEngine::Client_onLoginFailed: failedcode=" + failedcode + "(" + serverErr(failedcode) +"), datas=" + _serverdatas.Length + " !");
+			Dbg.ERROR_MSG("KBEngine::Client_onLoginFailed: failedcode(" + failedcode + ":" + serverErr(failedcode) + "), datas(" + _serverdatas.Length + ")!");
 			Event.fireAll(EventOutTypes.onLoginFailed, failedcode);
 		}
 		
