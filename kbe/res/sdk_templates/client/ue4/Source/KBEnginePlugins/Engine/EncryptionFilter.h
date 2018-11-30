e6da932097c29de2f3ecc35012545779eb4cf590
jay602 <857024709@qq.com>
2018-11-30 13:52:59 +0800

    非源码版本UE4编译插件报cryptlib.lib link错误

--- kbe/res/sdk_templates/client/ue4/Source/KBEnginePlugins/Engine/EncryptionFilter.h
+++ kbe/res/sdk_templates/client/ue4/Source/KBEnginePlugins/Engine/EncryptionFilter.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include "KBECommon.h"
 
-#ifndef KBENGINE_NO_CRYPTO
+#ifndef  KBENGINE_NO_CRYPTO
 // https://stackoverflow.com/questions/51416259/unreal-engine-4-20-build-error-in-plugin-adaptive-unity-build-disabling-pch-f
 #pragma warning(disable:4668)   // x  is not defined as a preprocessor macro, replacing with '0' for '#if/#elif'
 #include "blowfish.h"
@@ -9,9 +9,8 @@
 #include "modes.h"
 #endif
 
-
 class MemoryStream;
-class PacketSenderBase;
+class PacketSender;
 class MessageReader;
 
 class EncryptionFilter
@@ -25,7 +24,7 @@ public:
 	virtual void decrypt(uint8 *buf, MessageLengthEx len) = 0;
 	virtual void decrypt(uint8 *buf, MessageLengthEx offset, MessageLengthEx len) = 0;
 
-	virtual bool send(PacketSenderBase* pPacketSender, MemoryStream *pPacket) = 0;
+	virtual bool send(PacketSender* pPacketSender, MemoryStream *pPacket) = 0;
 	virtual bool recv(MessageReader* pMessageReader, MemoryStream *pPacket) = 0;
 };
 
@@ -49,7 +48,7 @@ public:
 	virtual void decrypt(uint8 *buf, MessageLengthEx len);
 	virtual void decrypt(uint8 *buf, MessageLengthEx offset, MessageLengthEx len);
 
-	virtual bool send(PacketSenderBase *pPacketSender, MemoryStream *pPacket);
+	virtual bool send(PacketSender *pPacketSender, MemoryStream *pPacket);
 	virtual bool recv(MessageReader *pMessageReader, MemoryStream *pPacket);
 
 	TArray<uint8>& key() {
@@ -66,7 +65,7 @@ private:
 	MemoryStream*	pEncryptStream_;
 	MessageLength	packetLen_;
 	uint8			padSize_;
-#ifndef KBENGINE_NO_CRYPTO
+#ifndef  KBENGINE_NO_CRYPTO
 	CryptoPP::ECB_Mode<CryptoPP::Blowfish>::Encryption encripter;
 	CryptoPP::ECB_Mode<CryptoPP::Blowfish>::Decryption decripter;
 #endif
