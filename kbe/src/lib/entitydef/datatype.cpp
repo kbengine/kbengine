24b519bce13a69a77b73abc89e0234a7d2ca7f6c
jay602 <857024709@qq.com>
2018-12-13 14:58:58 +0800

    检查def默认值isVecotr功能调整

--- kbe/src/lib/entitydef/datatype.cpp
+++ kbe/src/lib/entitydef/datatype.cpp
@@ -14,7 +14,7 @@
 #include "pyscript/vector4.h"
 #include "pyscript/copy.h"
 #include "pyscript/py_memorystream.h"
-#include <regex>
+#include <cctype>
 
 #ifndef CODE_INLINE
 #include "datatype.inl"
@@ -50,16 +50,17 @@ static bool isVecotr(std::string str, std::size_t n, std::vector<float>& nums)
 	}
 
 	nums.clear();
-	std::regex r("^[-\\+]?[\\d]+(\\.[\\d]+)?$");
 	for (auto ite = result.begin(); ite != result.end(); ite++)
 	{
-		if (!std::regex_match(*ite, r))
+		try
+		{
+			float num = 0.f;
+			StringConv::str2value(num, (*ite).c_str());
+			nums.push_back(num);
+		}
+		catch (...)
 		{
 			return false;
-		}
-		else
-		{
-			nums.push_back(std::stof(*ite));
 		}
 	}
 
