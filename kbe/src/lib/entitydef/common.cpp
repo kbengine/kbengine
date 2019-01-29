9191f2710cb3d1631e3baaad7ed69e6386c7603b
kebiao <380000937@qq.com>
2019-01-29 14:41:04 +0800

    up

--- kbe/src/lib/entitydef/common.cpp
+++ kbe/src/lib/entitydef/common.cpp
@@ -2,6 +2,8 @@
 
 
 #include "common.h"
+#include "resmgr/resmgr.h"
+
 namespace KBEngine{
 
 ENTITYFLAGMAP g_entityFlagMapping;
@@ -36,5 +38,63 @@ std::string entityDataFlagsToString(uint32 flags)
 	return "UNKOWN";
 }
 
+//-------------------------------------------------------------------------------------
+std::pair<std::wstring, std::wstring> getComponentPythonPaths(COMPONENT_TYPE componentType)
+{
+	std::pair<std::wstring, std::wstring> pathPair = std::make_pair(L"", L"");
+
+	std::wstring user_scripts_path = L"";
+	wchar_t* tbuf = KBEngine::strutil::char2wchar(const_cast<char*>(Resmgr::getSingleton().getPyUserScriptsPath().c_str()));
+	if (tbuf != NULL)
+	{
+		user_scripts_path += tbuf;
+		free(tbuf);
+	}
+	else
+	{
+		return pathPair;
+	}
+
+	std::wstring pyPaths = user_scripts_path + L";";
+	pyPaths += user_scripts_path + L"common;";
+	pyPaths += user_scripts_path + L"data;";
+	pyPaths += user_scripts_path + L"user_type;";
+
+	switch (componentType)
+	{
+	case BASEAPP_TYPE:
+		pyPaths += user_scripts_path + L"server_common;";
+		pyPaths += user_scripts_path + L"base;";
+		pyPaths += user_scripts_path + L"base/interfaces;";
+		pyPaths += user_scripts_path + L"base/components;";
+		break;
+	case CELLAPP_TYPE:
+		pyPaths += user_scripts_path + L"server_common;";
+		pyPaths += user_scripts_path + L"cell;";
+		pyPaths += user_scripts_path + L"cell/interfaces;";
+		pyPaths += user_scripts_path + L"cell/components;";
+		break;
+	case DBMGR_TYPE:
+		pyPaths += user_scripts_path + L"server_common;";
+		pyPaths += user_scripts_path + L"db;";
+		break;
+	default:
+		pyPaths += user_scripts_path + L"client;";
+		pyPaths += user_scripts_path + L"client/interfaces;";
+		pyPaths += user_scripts_path + L"client/components;";
+		break;
+	};
+
+	std::string kbe_res_path = Resmgr::getSingleton().getPySysResPath();
+	kbe_res_path += "scripts/common";
+
+	tbuf = KBEngine::strutil::char2wchar(const_cast<char*>(kbe_res_path.c_str()));
+	pathPair.first = tbuf;
+	pathPair.second = pyPaths;
+	free(tbuf);
+	return pathPair;
+}
+
+//-------------------------------------------------------------------------------------
 
 }
