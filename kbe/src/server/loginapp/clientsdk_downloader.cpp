7188dcbb963d8a2980340690fa8e63d2f80a731c
jay602 <857024709@qq.com>
2019-01-30 19:43:22 +0800

    动态更新静态sdk代码linux测试以及表现完善 #990 https://github.com/kbengine/kbengine/issues/990

--- kbe/src/server/loginapp/clientsdk_downloader.cpp
+++ kbe/src/server/loginapp/clientsdk_downloader.cpp
@@ -163,9 +163,10 @@ bool ClientSDKDownloader::loadSDKDatas()
 	uint64 now = timestamp();
 	if (TimeStamp::toSeconds(now - lastTime_) <= 0.1)
 	{
-		lastTime_ = now;
 		return false;
 	}
+
+	lastTime_ = now;
 
 	if (!loadedSDK_)
 	{
