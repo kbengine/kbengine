af91eb0d0790786782c5191c4f265ea446c1f7ff
kebiao <380000937@qq.com>
2019-01-31 11:32:39 +0800

    修正cellentitycall（Cell通过Base类）在base上也被自动转为了baseentity
    https://github.com/kbengine/kbengine/issues/1036

--- kbe/src/lib/common/common.h
+++ kbe/src/lib/common/common.h
@@ -50,42 +50,6 @@ enum ACCOUNT_FLAGS
 	ACCOUNT_FLAG_NOT_ACTIVATED = 0x000000002
 };
 
-/** entity的entityCall类别 */
-enum ENTITYCALL_TYPE
-{
-	ENTITYCALL_TYPE_CELL												= 0,
-	ENTITYCALL_TYPE_BASE												= 1,
-	ENTITYCALL_TYPE_CLIENT												= 2,
-	ENTITYCALL_TYPE_CELL_VIA_BASE										= 3,
-	ENTITYCALL_TYPE_BASE_VIA_CELL										= 4,
-	ENTITYCALL_TYPE_CLIENT_VIA_CELL										= 5,
-	ENTITYCALL_TYPE_CLIENT_VIA_BASE										= 6,
-};
-
-/** entityCall的类别对换为字符串名称 严格和ENTITYCALL_TYPE索引匹配 */
-const char ENTITYCALL_TYPE_TO_NAME_TABLE[][8] = 
-{
-	"cell",
-	"base",
-	"client",
-	"cell",
-	"base",
-	"client",
-	"client",
-};
-
-/** entityCall的类别对换为字符串名称 严格和ENTITYCALL_TYPE索引匹配 */
-const char ENTITYCALL_TYPE_TO_NAME_TABLE_EX[][14] =
-{
-	"cell",
-	"base",
-	"client",
-	"cellViaBase",
-	"baseViaCell",
-	"clientViaCell",
-	"clientViaBase",
-};
-
 /** 定义服务器各组件状态 */
 enum COMPONENT_STATE
 {
@@ -314,6 +278,68 @@ const COMPONENT_CLIENT_TYPE ALL_CLIENT_TYPES[] = {CLIENT_TYPE_MOBILE, CLIENT_TYP
 												CLIENT_TYPE_BROWSER, CLIENT_TYPE_BOTS, CLIENT_TYPE_MINI, UNKNOWN_CLIENT_COMPONENT_TYPE};
 
 typedef int8 CLIENT_CTYPE;
+
+/** entity的entityCall类别 */
+enum ENTITYCALL_TYPE
+{
+	ENTITYCALL_TYPE_CELL = 0,
+	ENTITYCALL_TYPE_BASE = 1,
+	ENTITYCALL_TYPE_CLIENT = 2,
+	ENTITYCALL_TYPE_CELL_VIA_BASE = 3,
+	ENTITYCALL_TYPE_BASE_VIA_CELL = 4,
+	ENTITYCALL_TYPE_CLIENT_VIA_CELL = 5,
+	ENTITYCALL_TYPE_CLIENT_VIA_BASE = 6,
+};
+
+/** 通过entityCall的类别获得该entity对应的组件类型 */
+inline COMPONENT_TYPE entityCallType2ComponentType(ENTITYCALL_TYPE type)
+{
+	switch (type)
+	{
+	case ENTITYCALL_TYPE_CELL:
+		return CELLAPP_TYPE;
+	case ENTITYCALL_TYPE_BASE:
+		return BASEAPP_TYPE;
+	case ENTITYCALL_TYPE_CLIENT:
+		return CLIENT_TYPE;
+	case ENTITYCALL_TYPE_CELL_VIA_BASE:
+		return CELLAPP_TYPE;
+	case ENTITYCALL_TYPE_BASE_VIA_CELL:
+		return BASEAPP_TYPE;
+	case ENTITYCALL_TYPE_CLIENT_VIA_CELL:
+		return CLIENT_TYPE;
+	case ENTITYCALL_TYPE_CLIENT_VIA_BASE:
+		return CLIENT_TYPE;
+	default:
+		break;
+	};
+
+	return UNKNOWN_COMPONENT_TYPE;
+};
+
+/** entityCall的类别对换为字符串名称 严格和ENTITYCALL_TYPE索引匹配 */
+const char ENTITYCALL_TYPE_TO_NAME_TABLE[][8] =
+{
+	"cell",
+	"base",
+	"client",
+	"cell",
+	"base",
+	"client",
+	"client",
+};
+
+/** entityCall的类别对换为字符串名称 严格和ENTITYCALL_TYPE索引匹配 */
+const char ENTITYCALL_TYPE_TO_NAME_TABLE_EX[][14] =
+{
+	"cell",
+	"base",
+	"client",
+	"cellViaBase",
+	"baseViaCell",
+	"clientViaCell",
+	"clientViaBase",
+};
 
 /*
  APP设置的标志
