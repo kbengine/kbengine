a3d45a491871f1081aae42859af13a932e9bfbaf
jay602 <857024709@qq.com>
2019-02-21 15:56:42 +0800

    Linux平台下遇到目录有空格的情况下，shell脚本执行有问题 #693

--- kbe/res/sdk_templates/server/python_assets/start_server.sh
+++ kbe/res/sdk_templates/server/python_assets/start_server.sh
@@ -8,7 +8,7 @@ bcontain=`echo $currPath|grep $keyStr|wc -l`
 
 if [ $bcontain = 0 ]
 then
-	export KBE_ROOT=$(cd ../; pwd)
+	export KBE_ROOT="$(cd ../; pwd)"
 else
 	export KBE_ROOT="$(pwd | awk -F "/kbengine/" '{print $1}')/kbengine"
 fi
@@ -24,12 +24,12 @@ echo KBE_BIN_PATH = \"${KBE_BIN_PATH}\"
 
 sh ./kill_server.sh
 
-$KBE_BIN_PATH/machine --cid=2129652375332859700 --gus=1&
-$KBE_BIN_PATH/logger --cid=1129653375331859700 --gus=2&
-$KBE_BIN_PATH/interfaces --cid=1129652375332859700 --gus=3&
-$KBE_BIN_PATH/dbmgr --cid=3129652375332859700 --gus=4&
-$KBE_BIN_PATH/baseappmgr --cid=4129652375332859700 --gus=5&
-$KBE_BIN_PATH/cellappmgr --cid=5129652375332859700 --gus=6&
-$KBE_BIN_PATH/baseapp --cid=6129652375332859700 --gus=7&
-$KBE_BIN_PATH/cellapp --cid=7129652375332859700 --gus=8&
-$KBE_BIN_PATH/loginapp --cid=8129652375332859700 --gus=9&
+"$KBE_BIN_PATH/machine" --cid=2129652375332859700 --gus=1&
+"$KBE_BIN_PATH/logger" --cid=1129653375331859700 --gus=2&
+"$KBE_BIN_PATH/interfaces" --cid=1129652375332859700 --gus=3&
+"$KBE_BIN_PATH/dbmgr" --cid=3129652375332859700 --gus=4&
+"$KBE_BIN_PATH/baseappmgr" --cid=4129652375332859700 --gus=5&
+"$KBE_BIN_PATH/cellappmgr" --cid=5129652375332859700 --gus=6&
+"$KBE_BIN_PATH/baseapp" --cid=6129652375332859700 --gus=7&
+"$KBE_BIN_PATH/cellapp" --cid=7129652375332859700 --gus=8&
+"$KBE_BIN_PATH/loginapp" --cid=8129652375332859700 --gus=9&
