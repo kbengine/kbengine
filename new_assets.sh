262b28c8284159ada5f7e66c29104b9113095496
jay602 <857024709@qq.com>
2019-02-21 15:19:52 +0800

    Linux平台下遇到目录有空格的情况下，shell脚本执行有问题 #693

--- new_assets.sh
+++ new_assets.sh
@@ -12,7 +12,7 @@ then
 else
 	export KBE_ROOT="$(pwd | awk -F "/kbengine" '{print $1}')/kbengine"
 fi
-
+echo $KBE_ROOT
 export KBE_RES_PATH="$KBE_ROOT/kbe/res"
 export KBE_BIN_PATH="$KBE_ROOT/kbe/bin/server"
 
@@ -20,4 +20,4 @@ echo KBE_ROOT = \"${KBE_ROOT}\"
 echo KBE_RES_PATH = \"${KBE_RES_PATH}\"
 echo KBE_BIN_PATH = \"${KBE_BIN_PATH}\"
 
-"$KBE_BIN_PATH/kbcmd" --newassets=python --outpath=$currPath/server_assets
+"$KBE_BIN_PATH/kbcmd" --newassets=python --outpath="$currPath/server_assets"
