84c646fb109acfc420f66458d0343f8cc41e7085
kebiao <380000937@qq.com>
2019-03-28 16:52:04 +0800

    up

--- kbe/src/lib/server/components.cpp
+++ kbe/src/lib/server/components.cpp
@@ -326,11 +326,11 @@ void Components::removeComponentByChannel(Network::Channel * pChannel, bool isSh
 
 				if (!isShutingdown && g_componentType != LOGGER_TYPE && g_componentType != INTERFACES_TYPE)
 				{
-					ERROR_MSG(fmt::format("Components::removeComponentByChannel: {} : {}, Abnormal exit! {}\n",
-						COMPONENT_NAME_EX(componentType), (*iter).cid, pChannel->condemnReason()));
+					ERROR_MSG(fmt::format("Components::removeComponentByChannel: {} : {}, Abnormal exit(reason={})! Channel(timestamp={}, lastReceivedTime={}, inactivityExceptionPeriod={})\n",
+						COMPONENT_NAME_EX(componentType), (*iter).cid, pChannel->condemnReason(), timestamp(), pChannel->lastReceivedTime(), pChannel->inactivityExceptionPeriod()));
 
 #if KBE_PLATFORM == PLATFORM_WIN32
-					printf("[ERROR]: %s.\n", (fmt::format("Components::removeComponentByChannel: {} : {}, Abnormal exit! {}\n",
+					printf("[ERROR]: %s.\n", (fmt::format("Components::removeComponentByChannel: {} : {}, Abnormal exit(reason={})!\n",
 						COMPONENT_NAME_EX(componentType), (*iter).cid, pChannel->condemnReason())).c_str());
 #endif
 				}
