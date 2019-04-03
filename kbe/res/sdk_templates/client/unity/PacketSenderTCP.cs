50821a215d502468b0412b272c3fc73185d8cf25
jay602 <857024709@qq.com>
2019-04-03 18:39:32 +0800

    monitor的参数要用引用类型

--- kbe/res/sdk_templates/client/unity/PacketSenderTCP.cs
+++ kbe/res/sdk_templates/client/unity/PacketSenderTCP.cs
@@ -24,8 +24,8 @@
 		int _wpos = 0;				// 写入的数据位置
 		int _spos = 0;				// 发送完毕的数据位置
 
-        object _sendingObj = new object();
-        Boolean _sending = false;
+		object _sendingObj = new object();
+		Boolean _sending = false;
 		
 		public PacketSenderTCP(NetworkInterfaceBase networkInterface) : base(networkInterface)
 		{
