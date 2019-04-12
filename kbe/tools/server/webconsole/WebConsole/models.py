97cc095178539799e53912bbb3e46e73498779dd
jay602 <857024709@qq.com>
2019-04-12 12:25:29 +0800

    pycommon 代码格式化 #1139

--- kbe/tools/server/webconsole/WebConsole/models.py
+++ kbe/tools/server/webconsole/WebConsole/models.py
@@ -17,21 +17,21 @@ CREATE TABLE `User` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 	"""
-	id                      = models.AutoField(primary_key = True)
-	name                    = models.CharField(max_length = 128, default = "", help_text = "账号", db_index = True, unique = True)
-	show_name               = models.CharField(max_length = 128, default = "", help_text = "显示名")
-	password                = models.CharField(max_length = 128, default = "", help_text = "密码")
-	sys_user                = models.CharField(max_length = 128, default = "", help_text = "系统账号")
-	sys_uid                 = models.IntegerField(default = 0, help_text = "系统账号ID")
-	kbe_root                = models.CharField(max_length = 256, default = "", help_text = "kbe_root")
-	kbe_res_path            = models.CharField(max_length = 256, default = "", help_text = "kbe_res_path")
-	kbe_bin_path            = models.CharField(max_length = 256, default = "", help_text = "kbe_bin_path")
+	id						= models.AutoField(primary_key = True)
+	name					= models.CharField(max_length = 128, default = "", help_text = "账号", db_index = True, unique = True)
+	show_name				= models.CharField(max_length = 128, default = "", help_text = "显示名")
+	password				= models.CharField(max_length = 128, default = "", help_text = "密码")
+	sys_user				= models.CharField(max_length = 128, default = "", help_text = "系统账号")
+	sys_uid					= models.IntegerField(default = 0, help_text = "系统账号ID")
+	kbe_root				= models.CharField(max_length = 256, default = "", help_text = "kbe_root")
+	kbe_res_path			= models.CharField(max_length = 256, default = "", help_text = "kbe_res_path")
+	kbe_bin_path			= models.CharField(max_length = 256, default = "", help_text = "kbe_bin_path")
 
 class ServerLayout( models.Model ):
 	"""
 	服务器运行配置表
 	"""
-	id         = models.AutoField(primary_key = True)
-	name       = models.CharField(max_length = 128, default = "", help_text = "名称", db_index = True, unique = True)
-	sys_user   = models.CharField(max_length = 128, default = "", help_text = "系统账号")
-	config     = models.TextField(max_length = 32768, default = "", help_text = "配置(JSON)")
+	id			= models.AutoField(primary_key = True)
+	name		= models.CharField(max_length = 128, default = "", help_text = "名称", db_index = True, unique = True)
+	sys_user	= models.CharField(max_length = 128, default = "", help_text = "系统账号")
+	config		= models.TextField(max_length = 32768, default = "", help_text = "配置(JSON)")
