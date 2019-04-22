51cddb2f36246d903e4e22ea54ef498e263699a9
kebiao <380000937@qq.com>
2019-04-22 11:58:56 +0800

    v2.5.0
    主要增加了直接在py代码中定义实体

--- kbe/src/lib/common/kbeversion.h
+++ kbe/src/lib/common/kbeversion.h
@@ -8,8 +8,8 @@
 namespace KBEngine{
 	
 #define KBE_VERSION_MAJOR 2
-#define KBE_VERSION_MINOR 4
-#define KBE_VERSION_PATCH 5
+#define KBE_VERSION_MINOR 5
+#define KBE_VERSION_PATCH 0
 
 
 namespace KBEVersion
